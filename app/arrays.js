a17ebc980880356b993fc706bcc5e958dbc6c6df
Aaron <a.albinson@teluxhd.com>
2018-07-03 09:57:48 +0100

    Add changes

--- app/arrays.js
+++ app/arrays.js
@@ -1,59 +1,46 @@
-exports = typeof window === 'undefined' ? global : window;
+exports = typeof window === "undefined" ? global : window;
 
 exports.arraysAnswers = {
-  indexOf: function(arr, item) {
-
-  },
+  indexOf: function(arr, item) {},
 
   sum: function(arr) {
-
+    return arr.reduce(function(a, b) {
+      return a + b;
+    });
   },
 
   remove: function(arr, item) {
-
+    return arr.filter(x => x != item);
   },
 
-  removeWithoutCopy: function(arr, item) {
-
-  },
+  removeWithoutCopy: function(arr, item) {},
 
   append: function(arr, item) {
-
+    arr.push(item);
+    return arr;
   },
 
   truncate: function(arr) {
-
+    arr.pop(item);
+    return arr;
   },
 
   prepend: function(arr, item) {
-
+    arr.unshift(item);
+    return arr;
   },
 
-  curtail: function(arr) {
+  curtail: function(arr) {},
 
-  },
+  concat: function(arr1, arr2) {},
 
-  concat: function(arr1, arr2) {
+  insert: function(arr, item, index) {},
 
-  },
+  count: function(arr, item) {},
 
-  insert: function(arr, item, index) {
+  duplicates: function(arr) {},
 
-  },
+  square: function(arr) {},
 
-  count: function(arr, item) {
-
-  },
-
-  duplicates: function(arr) {
-
-  },
-
-  square: function(arr) {
-
-  },
-
-  findAllOccurrences: function(arr, target) {
-
-  }
+  findAllOccurrences: function(arr, target) {}
 };
